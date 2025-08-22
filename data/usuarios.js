22c09cc7f2c3113e44885e42c4d71d3e8b8b58f0
RedAkaAguilar <pandagamer7553@outlook.com>
2025-08-22 14:48:04 -0600

    Create usuarios.js

---
+++ data/usuarios.js
@@ -0,0 +1,10 @@
+window.remoteUsers = {
+  "admin@mcstellar.com": {
+    password: "admin123",
+    redirect: "admin.html"
+  },
+  "nuevo@mcstellar.com": {
+    password: "nuevo2025",
+    redirect: "nuevo.html"
+  }
+};
