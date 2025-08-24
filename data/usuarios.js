5a0706c72c7441fe4c23871a0b8d5f4602c5abbb
RedAkaAguilar <pandagamer7553@outlook.com>
2025-08-23 20:34:58 -0600

    Update usuarios.js

--- data/usuarios.js
+++ data/usuarios.js
@@ -2,11 +2,11 @@ window.remoteUsers = {
   "zyrakaaguilar@mcstellar.com": {
     username: "AdminMaster",
     password: "Cyn201030",
-    redirect: "admin.html"
+    redirect: "https://app.mcstellarstudio.com/@me"
   },
   "ganga@mcstellar.com": {
     username: "AdminMaster",
     password: "angel1fa",
-    redirect: "nuevo.html"
+    redirect: "https://app.mcstellarstudio.com/@me"
   }
 };
