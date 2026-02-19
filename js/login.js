1e199164a487cf9173e15b7afc0d5a34cf7605d2
RedAkaAguilar <pandagamer7553@outlook.com>
2026-02-18 20:12:35 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,26 @@
+import { setSession } from "./auth.js";
+
+async function login() {
+  const input = document.getElementById("username").value;
+
+  const res = await fetch("/data/users.json");
+  const data = await res.json();
+
+  const user = data.users.find(u => u.username === input);
+
+  if (!user) {
+    alert("Usuario no encontrado");
+    return;
+  }
+
+  if (user.level === 0) {
+    alert("Estás baneado.");
+    return;
+  }
+
+  setSession(user);
+  window.location.href = "/index.html";
+}
+
+window.login = login;
 
